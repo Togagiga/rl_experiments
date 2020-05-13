fixed rotation around centre and added drag

racecar.py
@@ -10,14 +10,18 @@ import pygame as pg
 
 
 pg.init()
-size = width, height = 700, 700
+size = width, height = 1200, 1200
 win = pg.display.set_mode(size)
 pg.display.set_caption('Car Race Game')
 
 road_width = 10000
 car_img = pg.image.load('assets/car-top-view.png').convert_alpha()  # loading car image
-car_width, car_height = 50, 100                                        # specificing scaling size
+car_width, car_height = 50, 100                                       # specificing scaling size
 car_img = pg.transform.scale(car_img, (car_width,car_height))       # scaling car image
+
+vel_inc = 0.4
+theta_inc = 4
+drag_const = 0.001
 
 GREEN = (0, 138, 55)   # RGB Colours
 GREY = (67, 67, 67)
@@ -27,9 +31,9 @@ clock = pg.time.Clock()
 class Map():
 
     def __init__(self):
-
+        self.tilesize = 10
         self.map = np.zeros((int(width/self.tilesize), int(height/self.tilesize)), dtype = int)
-
+        
         shape = self.map.shape
         self.map[:,int(shape[0]/2-road_width):int(shape[1]/2+road_width)] = 1   # make road of ones
 
@@ -65,9 +69,11 @@ def rot_center(img, theta):
 class Car():
 
     # need to initialise car position --> need to occupy whole number of squares
-    def __init__(self, x=width/2-car_width/2, y=height/2 - car_height/2, width=car_width, height=car_height, vel = 0, theta = 0):
+    def __init__(self, x=width/2-car_width/2, y=height/2 - car_height/2, width=car_width, height=car_height, vel = 0, theta = 0 , pos_x = 0, pos_y = 0):
         self.x = int(x)
         self.y = int(y)
+        self.pos_x = pos_x
+        self.pos_y = pos_y
         self.theta = theta
         self.stationary = True # initially not moving (before player input)
         self.left = False
@@ -89,11 +95,10 @@ class Car():
             return True
 
     def draw(self):
-        # add conditionals for different poses of car
-        self.y -= np.cos(self.theta*(2*np.pi)/360)*self.vel
-        self.x -= np.sin(self.theta*(2*np.pi)/360)*self.vel
-        img = rot_center(car_img, self.theta)
-        win.blit(img, (self.x, self.y))
+        self.y -= np.cos(self.theta*(2*np.pi)/360)*(self.vel)              #calculates new x,y coordinates based on current x,y,vel,theta 
+        self.x -= np.sin(self.theta*(2*np.pi)/360)*self.vel               
+        img = rot_center(car_img, self.theta)                              #calls function to rotate image around centre point
+        win.blit(img, (self.x - (img.get_rect().width - car_width)/2, self.y - (img.get_rect().height - car_height)/2)) #subtracting from x and y to ensure smooth rotation
 
 
 def redrawGameWindow():
@@ -118,26 +123,34 @@ while run:
 
     keys = pg.key.get_pressed()
 
-    if keys[pg.K_LEFT]:
-        car.theta += 5
-    elif keys[pg.K_RIGHT]:
-        car.theta -= 5
-    elif keys[pg.K_UP]:
-        car.vel += 0.5
-    elif keys[pg.K_UP] and keys[pg.K_LEFT]:
-        car.vel += 0.5
-        car.theta += 5
+
+    if keys[pg.K_UP] and keys[pg.K_LEFT]:
+        car.vel += vel_inc
+        car.theta += theta_inc
     elif keys[pg.K_UP] and keys[pg.K_RIGHT]:
-        car.vel += 0.5
-        car.theta -= 5
+        car.vel += vel_inc
+        car.theta -= theta_inc
     elif keys[pg.K_DOWN] and keys[pg.K_LEFT]:
-        car.vel -= 0.5
-        car.theta += 5
+        car.vel -= vel_inc
+        car.theta += theta_inc
     elif keys[pg.K_DOWN] and keys[pg.K_RIGHT]:
-        car.vel -= 0.5
-        car.theta -= 5
+        car.vel -= vel_inc
+        car.theta -= theta_inc
     elif keys[pg.K_DOWN]:
-        car.vel -= 0.5
+        car.vel -= vel_inc
+    elif keys[pg.K_LEFT]:
+        car.theta += theta_inc
+    elif keys[pg.K_RIGHT]:
+        car.theta -= theta_inc
+    elif keys[pg.K_UP]:
+        car.vel += vel_inc
+    
+    #function to produce drag effect
+    if car.vel >= 0: 
+        car.vel -= drag_const*np.square(car.vel)     
+    else:
+        car.vel += drag_const*np.square(car.vel)  
+    
     redrawGameWindow()
 
 pg.quit()