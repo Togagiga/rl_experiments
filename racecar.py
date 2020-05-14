4c9bf16c05d8ef69d410fe5a3a21f758ffeee115
Jonas Dieker <jonasdieker05@gmail.com>
2020-05-14 15:23:06 +0200

    added visuliation for true image size + fixed constraints

--- racecar.py
+++ racecar.py
@@ -16,7 +16,7 @@ pg.display.set_caption('Car Race')
 
 road_width = 20
 car_img = pg.image.load('assets/car-top-view.png').convert_alpha()  # loading car image
-car_width, car_height = 50, 100                                       # specificing scaling size
+car_width, car_height = 50, 100                                      # specificing scaling size
 car_img = pg.transform.scale(car_img, (car_width,car_height))       # scaling car image
 
 vel_inc = 0.4
@@ -25,6 +25,7 @@ drag_const = 0.001
 
 GREEN = (0, 138, 55)   # RGB Colours
 GREY = (67, 67, 67)
+RED = (253, 8, 8)
 
 clock = pg.time.Clock()
 
@@ -62,9 +63,6 @@ class Car():
         self.x = int(x)
         self.y = int(y)
         self.theta = theta
-        self.stationary = True # initially not moving (before player input)
-        self.left = False
-        self.right = False
         self.vel = vel          # how far car moves (in pixels) with each press of a button
 
     def update_pos(self, vel, theta):
@@ -73,25 +71,22 @@ class Car():
 
     def checkLegalMove(self, vel, theta):
 
-        #self.update_pos(vel, theta)
-
-        self.y -= np.cos(theta*(2*np.pi)/360)*vel              #calculates new x,y coordinates based on current x,y,vel,theta 
-        self.x -= np.sin(theta*(2*np.pi)/360)*vel
-
-        img_check = rot_center(car_img, theta)
+        self.update_pos(vel, theta)
+        img_check = rot_center(car_img, theta)   # rotating image of car
 
         car_h = img_check.get_rect().height   # rotated size of car
-        print(car_h)
         car_w = img_check.get_rect().width
-        print(car_w)
+
+        check_x = self.x - (car_w - car_width)/2      # position account for car turning
+        check_y = self.y - (car_h - car_height)/2
         
-        map_y = math.floor(self.y/Map().tilesize)     # position in tiles
-        map_x = math.floor(self.x/Map().tilesize)
+        map_y = math.floor(check_y/Map().tilesize)     # position in tiles
+        map_x = math.floor(check_x/Map().tilesize)
 
         car_h_tiles = math.ceil(car_h/Map().tilesize)  # getting car size in tiles
         car_w_tiles = math.ceil(car_w/Map().tilesize)
 
-        check =  np.argwhere(Map().map[map_y:map_y + car_h_tiles, map_x:map_x + car_w_tiles] == 0)  # if car is on zeros
+        check =  np.argwhere(Map().map[map_y:map_y + car_h_tiles, map_x:map_x + car_w_tiles] == 0)  # if car is on zeros on map
         if len(check) != 0:
             print('crashed')
             return False
@@ -103,8 +98,11 @@ class Car():
     def draw(self):
         self.y -= np.cos(self.theta*(2*np.pi)/360)*self.vel              #calculates new x,y coordinates based on current x,y,vel,theta 
         self.x -= np.sin(self.theta*(2*np.pi)/360)*self.vel              
-        img = rot_center(car_img, self.theta)                              #calls function to rotate image around centre point
+        img = rot_center(car_img, self.theta)                            #calls function to rotate image around centre point
+        # drawing retangle representing actual car width and height
+        pg.draw.rect(win, RED, (self.x - (img.get_rect().width - car_width)/2, self.y - (img.get_rect().height - car_height)/2, img.get_rect().width, img.get_rect().height))
         win.blit(img, (self.x - (img.get_rect().width - car_width)/2, self.y - (img.get_rect().height - car_height)/2)) #subtracting from x and y to ensure smooth rotation
+
 
 
 def redrawGameWindow():
@@ -128,11 +126,6 @@ while run:
 
 
     keys = pg.key.get_pressed()
-
-    if not car.checkLegalMove(car.vel, car.theta):
-        run = False
-    else: 
-        pass
 
     if keys[pg.K_UP] and keys[pg.K_LEFT]:
         car.vel += vel_inc
@@ -162,5 +155,10 @@ while run:
         car.vel += drag_const*np.square(car.vel)
     
     redrawGameWindow()
+
+    if not car.checkLegalMove(car.vel, car.theta):     # checks whether car is on road or not
+        run = False
+    else: 
+        pass
 
 pg.quit()
